bcda411bd60c415b5c8b32eb1526fdb24a33aa8a
meherab111 <mehrabborno@gmail.com>
2025-04-07 01:49:15 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -4,11 +4,19 @@ const closeIcon = document.getElementById("close-icon")
 
 const navBar = document.getElementById("navbar")
 
+const overlayDiv = document.getElementById("overlay-div")
+
 openIcon.addEventListener("click", () => {
     navBar.classList.add("show")
+    overlayDiv.classList.add("overlay")
 })
 
-
 closeIcon.addEventListener("click", () => {
+    overlayDiv.classList.remove("overlay")
+    navBar.classList.remove("show")
+})
+
+overlayDiv.addEventListener("click", () => {
+    overlayDiv.classList.remove("overlay")
     navBar.classList.remove("show")
 })
